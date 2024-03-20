28c1d98ad8bd9721602575f04443005c96c17c13
ethioendu <117819326+ethioendu@users.noreply.github.com>
2024-03-20 15:24:58 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,59 +1,47 @@
 #!/usr/bin/python3
 """ """
 from models.base_model import BaseModel
+import unittest
 import datetime
-import pycodestyle
+from uuid import UUID
 import json
 import os
-import unittest
-from uuid import UUID
-
-from models.base_model import BaseModel, Base
+import pycodestyle
 
 
-class TestBasemodel(unittest.TestCase):
-    """Represents the tests for the BaseModel."""
+class test_basemodel(unittest.TestCase):
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """Initializes the test class."""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """Performs some operations before the tests are run."""
+        """ """
         pass
 
     def tearDown(self):
-        """Performs some operations after the tests are run"""
         try:
             os.remove('file.json')
-        except :
+        except:
             pass
 
     def test_default(self):
-        """Tests the initialization of the model class.
-        """
+        """ """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
-    def test_default(self):
-        """Tests the type of value stored."""
-        i = self.value()
-        copy = i.to_dict()
-        new = BaseModel(**copy)
-        self.assertFalse(new is i)
-        
-
     def test_kwargs(self):
-        """Tests kwargs with an int."""
+        """ """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """Tests kwargs with an int."""
+        """ """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -61,7 +49,7 @@ class TestBasemodel(unittest.TestCase):
             new = BaseModel(**copy)
 
     def test_save(self):
-        """Tests the save function of the BaseModel class."""
+        """ Testing save """
         i = self.value()
         i.save()
         key = self.name + "." + i.id
@@ -70,17 +58,24 @@ class TestBasemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """Tests the __str__ function of the BaseModel class."""
+        """ """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """Tests the to_dict function of the model class."""
+        """ """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
-         def test_kwargs_one(self):
+
+    def test_kwargs_none(self):
+        """ """
+        n = {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**n)
+
+    def test_kwargs_one(self):
         """ """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
