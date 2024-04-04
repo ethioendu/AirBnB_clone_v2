e581e83e738c7539862e93fc559d21df3cc3615d
Wasealex <wasealex27@gmail.com>
2024-04-05 01:25:17 +0300

    tria;3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,6 +12,25 @@ env.key_filename = '/home/wasealex/.ssh/ssh_key'
 env.user = 'ubuntu'
 
 
+def do_pack():
+    """ directory versions is created and web_static archive is saved
+        All archives is stored in folder versions
+        web_static_<year><month><day><hour><minute><second>.tgz is created
+        if the archive has been correctly generated return the archive path.
+        Otherwise, it should return None
+    """
+    local('sudo mkdir -p versions')
+    current_time = datetime.now()
+    time_str = current_time.strftime('%Y%m%d%H%M%S')
+    full_name = f'web_static_{time_str}.tgz'
+    result = local(f'sudo tar -cvzf versions/{full_name} web_static')
+    file_size = os.path.getsize('versions/' + full_name)
+    print(f"web_static packed: versions/{full_name} -> {file_size}Bytes")
+    if result.failed:
+        return None
+    return f'versions/{full_name}'
+
+
 def do_deploy(archive_path):
     """
     Distributes an archive to the web servers.
