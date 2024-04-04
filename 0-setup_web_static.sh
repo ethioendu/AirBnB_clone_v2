41068a6707748ca78040879b9be3dd6212a37337
Wasealex <wasealex27@gmail.com>
2024-04-04 13:36:32 +0300

    fixedS

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -31,20 +31,20 @@ for dir in "${directories[@]}"; do
     fi
 done
 
-sudo echo "
-<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -hR ubuntu:ubuntu /data/
 
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/
-{\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/{\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+
+sudo nginx -t
 
 sudo service nginx restart
